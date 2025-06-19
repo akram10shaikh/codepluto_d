7fed2ac5d30be5487aead065d54b472d86bb6723
akram <akram10shaikh@gmail.com>
2025-06-19 12:30:32 +0530

    staic root added

--- codepluto/settings.py
+++ codepluto/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
